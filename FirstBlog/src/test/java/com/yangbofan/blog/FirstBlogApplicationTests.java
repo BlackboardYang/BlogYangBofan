c66d7c4291ed28cc14f316839933de3690c6fcf6
LinearRegression <38725186+BlackboardYang@users.noreply.github.com>
2022-05-16 09:50:22 +0900

    First Publish
    First Publish

---
+++ FirstBlog/src/test/java/com/yangbofan/blog/FirstBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangbofan.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
