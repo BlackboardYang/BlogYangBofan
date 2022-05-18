ea46900fef02137381702b0da780a65532469491
LinearRegression <38725186+BlackboardYang@users.noreply.github.com>
2022-05-18 12:07:09 +0900

    post page finished

--- FirstBlog/src/main/resources/static/JavaScript/BlogScript.js
+++ FirstBlog/src/main/resources/static/JavaScript/BlogScript.js
@@ -1,24 +1,30 @@
+// 回到顶上ButtonScript
+	let topButton = document.getElementById("btn-back-to-top");
 
-//回到顶上button
-let topButton = document.getElementById("btn-back-to-top");
+	window.onscroll = function() {
+		scrollFunction();
+	};
 
-window.onscroll = function() {
-	scrollFunction();
-};
-
-function scrollFunction() {
-	if (
-		document.body.scrollTop > 20 ||
-		document.documentElement.scrollTop > 20
-	) {
-		topButton.style.display = "block";
-	} else {
-		topButton.style.display = "none";
+	function scrollFunction() {
+		if (document.body.scrollTop > 20
+				|| document.documentElement.scrollTop > 20) {
+			topButton.style.display = "block";
+		} else {
+			topButton.style.display = "none";
+		}
 	}
-}
-topButton.addEventListener("click", backToTop);
+	topButton.addEventListener("click", backToTop);
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+	function backToTop() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+
+// sidebarScript
+(() => {
+  'use strict'
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
